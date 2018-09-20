4cc7e0b15a6823a44f15ed2d0e5271570fbe1d05
volosgoto <volosgoto@yahoo.com>
2018-09-20 11:27:10 +0300

    show posts in DOM

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import { http } from './http';
+import { ui } from './ui';
 
 // Get Posts from DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
 function getPosts(params) {
     http.get('http://localhost:3000/posts')
-    .then(data => console.log(data))
+    // .then(data => console.log(data))
+    .then(data => ui.showPosts(data))
+    // .catch(err => console.log(err));
     .catch(err => console.log(err));
     
 }
---
+++ src/ui.js
@@ -0,0 +1,36 @@
+class Ui {
+    constructor(){
+        this.post = document.querySelector('#posts');
+        this.titleInput = document.querySelector('#title');
+        this.bodyInput = document.querySelector('#body');
+        this.idInput = document.querySelector('#id');
+        this.postSubmit = document.querySelector('.post-submit');
+        this.forState = 'add';
+    }
+
+    showPosts(posts){
+        // console.log(post);
+        let output = '';
+        posts.forEach((post) => {
+            output += `
+            <div class="card mb-3">
+              <div class="card-body">
+                <h4 class="card-title">${post.title}</h4>
+                <p class="card-text">${post.body}</p>
+                <a href="#" class="edit card-link" data-id="${post.id}">
+                  <i class="fa fa-pencil"></i>
+                </a>
+                <p class="card-text">${post.author}</p>
+    
+                <a href="#" class="delete card-link" data-id="${post.id}">
+                <i class="fa fa-remove"></i>
+              </a>
+              </div>
+            </div>
+          `;
+        });
+        this.post.innerHTML = output;
+   }
+}
+
+export const ui = new Ui();
