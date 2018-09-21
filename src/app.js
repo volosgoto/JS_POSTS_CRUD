e8e8a51344dc38e2689ed63b628b3ced67812a77
volosgoto <volosgoto@yahoo.com>
2018-09-21 09:37:22 +0300

    udp submitPost

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import { http } from './http';
 import { ui } from './ui';
 
-// Get posts on DOM load
+// Listen Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
+
+// Listen Add post
+document.querySelector('.post-submit')
+    .addEventListener('click', submitPost);
+
 
 function getPosts() {
   http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
     .catch(err => console.log(err));
 }
+
+
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+}
+
