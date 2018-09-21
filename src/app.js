ba113e5a4be95e82b59638b978bbfcf3e0282f82
volosgoto <volosgoto@yahoo.com>
2018-09-21 09:46:04 +0300

    Create Post

--- src/app.js
+++ src/app.js
@@ -20,5 +20,19 @@ function submitPost() {
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
 
+  const data = {
+    title,
+    body
+  }
+
+  // Create Post
+  http.post('http://localhost:3000/posts', data)
+    .then(data => {
+      ui.showAlert('Post added', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
+    .catch(err => console.log(err));
+
 }
 
